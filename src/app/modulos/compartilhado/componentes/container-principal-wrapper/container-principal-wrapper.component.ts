2878d5eb28f9edd69f98024b195cd9ec14deaf50
Abhner Araújo <afga@cin.ufpe.br>
2019-11-08 17:10:16 -0300

    feat: feat: ajustes no sidenav

--- src/app/modulos/compartilhado/componentes/container-principal-wrapper/container-principal-wrapper.component.ts
+++ src/app/modulos/compartilhado/componentes/container-principal-wrapper/container-principal-wrapper.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-container-principal-wrapper',
@@ -8,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 export class ContainerPrincipalWrapperComponent implements OnInit {
 
   sidenavEsquerdoAberto: boolean;
+  sidenavDireitoAberto: boolean;
+  sidenavEsquerdoMode: string;
 
-  constructor() {
+  mediaQuerySubscription: Subscription;
+  activeMediaQuery: string;
+
+  constructor(private mediaObserver: MediaObserver) {
     this.sidenavEsquerdoAberto = true;
+    this.sidenavDireitoAberto = true;
+    this.mediaQuerySubscription = this.mediaObserver.asObservable().subscribe(
+      (change: MediaChange[]) => {
+        this.activeMediaQuery = change[0].mqAlias;
+        if (this.activeMediaQuery === 'xs') {
+          this.sidenavDireitoAberto = false;
+          this.sidenavEsquerdoAberto = false;
+          this.sidenavEsquerdoMode = 'over';
+        } else {
+          this.sidenavEsquerdoMode = 'side';
+        }
+      });
   }
 
   ngOnInit() {

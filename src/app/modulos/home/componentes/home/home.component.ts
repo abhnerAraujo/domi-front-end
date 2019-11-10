675e5d43732a4eba85b52273ff6c893f68e90294
abhnerAraujo <afga@cin.ufpe.br>
2019-11-10 15:01:30 -0300

    feat: carregar nome do usuário no home

--- src/app/modulos/home/componentes/home/home.component.ts
+++ src/app/modulos/home/componentes/home/home.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  nomeUsuario: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.nomeUsuario = JSON.parse(localStorage.getItem('usuario')).nome;
   }
 
 }

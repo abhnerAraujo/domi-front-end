26225fa217cdb6790429805536f243b79d667c92
abhnerAraujo <afga@cin.ufpe.br>
2019-11-10 16:18:24 -0300

    feat: lista de pacientes

--- src/app/modulos/cadastros/componentes/paciente/paciente.module.ts
+++ src/app/modulos/cadastros/componentes/paciente/paciente.module.ts
@@ -1,10 +1,9 @@
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatDividerModule, MatButtonModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { PacienteRoutingModule } from './paciente-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PacientesListaComponent } from './pacientes-lista/pacientes-lista.component';
-
 
 
 @NgModule({
@@ -13,7 +12,9 @@ import { PacientesListaComponent } from './pacientes-lista/pacientes-lista.compo
     CommonModule,
     PacienteRoutingModule,
     FlexLayoutModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ]
 })
 export class PacienteModule { }

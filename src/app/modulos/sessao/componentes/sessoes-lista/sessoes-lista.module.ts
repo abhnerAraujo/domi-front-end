59486985b759e71f2bc767794c0dfd14b0e00123
Abhner Araújo <afga@cin.ufpe.br>
2019-12-10 08:55:58 -0300

    feat: progress bar na lista de sessão

--- src/app/modulos/sessao/componentes/sessoes-lista/sessoes-lista.module.ts
+++ src/app/modulos/sessao/componentes/sessoes-lista/sessoes-lista.module.ts
@@ -1,13 +1,19 @@
+import { PipesModule } from './../../../../pipes/pipes.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { SessoesListaRoutingModule } from './sessoes-lista-routing.module';
 import { SessoesListaComponent } from './sessoes-lista/sessoes-lista.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule({
   declarations: [SessoesListaComponent],
   imports: [
     CommonModule,
-    SessoesListaRoutingModule
+    SessoesListaRoutingModule,
+    FlexLayoutModule,
+    MatProgressSpinnerModule,
+    PipesModule
   ]
 })
 export class SessoesListaModule { }
--- src/app/modulos/sessao/componentes/sessoes-lista/sessoes-lista/sessoes-lista.component.ts
+++ src/app/modulos/sessao/componentes/sessoes-lista/sessoes-lista/sessoes-lista.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessoesListaComponent implements OnInit {
 
+  progresso: number;
+  totalPago: number;
+  total: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.total = 1040.00;
+    this.totalPago = 800.00;
+    this.carregarProgresso();
+  }
+
+  carregarProgresso() {
+    const porcentagemProgresso = Math.round((100 * this.totalPago) / this.total);
+    for (let i = 0; i <= porcentagemProgresso; i++) {
+      this.progresso = i;
+    }
   }
 
 }

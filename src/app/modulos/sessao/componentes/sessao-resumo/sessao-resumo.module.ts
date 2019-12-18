91f0e564e63694f2551e6f5ceae802b7b39983ac
Abhner Araujo <abhnerfelipe@gmail.com>
2019-12-18 16:24:46 -0300

    feat: lista itens resumo sessão

--- src/app/modulos/sessao/componentes/sessao-resumo/sessao-resumo.module.ts
+++ src/app/modulos/sessao/componentes/sessao-resumo/sessao-resumo.module.ts
@@ -2,12 +2,25 @@ import { SessaoResumoRoutingModule } from './sessao-resumo-routing.module';
 import { SessaoResumoComponent } from './sessao-resumo/sessao-resumo.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule, MatButtonModule, MatFormFieldModule, MatCheckboxModule, MatInputModule, MatDividerModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PipesModule } from 'src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [SessaoResumoComponent],
   imports: [
     CommonModule,
-    SessaoResumoRoutingModule
+    SessaoResumoRoutingModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
+    PipesModule
   ]
 })
 export class SessaoResumoModule { }

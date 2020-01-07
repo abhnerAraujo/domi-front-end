2840a572bf8553a37df78508c7473e2561a8bb7e
Abhner Araujo <abhnerfelipe@gmail.com>
2020-01-06 21:21:37 -0300

    feat: trocar espaco selecionado

--- src/app/modulos/compartilhado/compartilhado.module.ts
+++ src/app/modulos/compartilhado/compartilhado.module.ts
@@ -4,7 +4,9 @@ import { ContainerPrincipalWrapperComponent } from './componentes/container-prin
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ToolbarComponent } from './componentes/container-principal-wrapper/componentes/toolbar/toolbar.component';
-import { MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+import {
+  MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, MatMenuModule, MatDividerModule, MatProgressBarModule
+} from '@angular/material';
 import {
   NavegacaoMenuComponent
 } from './componentes/container-principal-wrapper/componentes/navegacao-menu/navegacao-menu.component';
@@ -19,7 +21,10 @@ import {
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatProgressBarModule
   ],
   exports: [
     ContainerPrincipalWrapperComponent

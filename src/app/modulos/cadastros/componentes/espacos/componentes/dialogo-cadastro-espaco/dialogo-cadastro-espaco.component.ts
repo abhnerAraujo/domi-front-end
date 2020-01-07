a4922c005ae4c07546aac41ce5432b03fd423348
Abhner Araujo <abhnerfelipe@gmail.com>
2020-01-06 23:16:23 -0300

    feat: modulo de espacos

---
+++ src/app/modulos/cadastros/componentes/espacos/componentes/dialogo-cadastro-espaco/dialogo-cadastro-espaco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialogo-cadastro-espaco',
+  templateUrl: './dialogo-cadastro-espaco.component.html',
+  styleUrls: ['./dialogo-cadastro-espaco.component.scss']
+})
+export class DialogoCadastroEspacoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

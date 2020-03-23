cdb1b8d908b77b4c72688f10e3db14a1e074b303
Abhner Araujo <abhnerfelipe@gmail.com>
2020-03-23 11:33:25 -0300

    refactor: padronização das listas de navegação

--- src/app/modulos/cadastros/componentes/cadastros-home/cadastros-home.module.ts
+++ src/app/modulos/cadastros/componentes/cadastros-home/cadastros-home.module.ts
@@ -1,6 +1,6 @@
 import { CadastrosHomeRoutingModule } from './cadastros-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatIconModule, MatButtonModule } from '@angular/material';
+import { MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
 import { CadastrosHomeComponent } from './componentes/cadastros-home/cadastros-home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,8 @@ import { CommonModule } from '@angular/common';
     CadastrosHomeRoutingModule,
     MatIconModule,
     MatButtonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ]
 })
 export class CadastrosHomeModule { }
